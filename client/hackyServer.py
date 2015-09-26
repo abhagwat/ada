93e92b7b5dfc133c51a7f5f67b48fda0879467e9
Apoorva Bhagwat <bhagwat_apoorva@hotmail.com>
2015-09-26 15:24:35 -0400

    bugsdoufgisue

--- client/hackyServer.py
+++ client/hackyServer.py
@@ -7,4 +7,9 @@ hackyServer = Flask(__name__)
 @hackyServer.route('/hack')
 def hack():
     import client
+    client.write("play")
+
+if __name__=='__main__':
+    hackyServer.run(port=80, host='0.0.0.0', debug=True)
+
 
